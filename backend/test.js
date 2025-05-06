bbbcaa14cbedb1b2e6cb4a72d92dec0fce64ae57
Curtis Chen <curtischen1234@gmail.com>
2025-05-05 22:57:27 -0700

    Complete backend functionality code

--- backend/test.js
+++ backend/test.js
@@ -38,19 +38,17 @@ async function analyze_pdf(pdf_path, context, task, output_constraint) {
   }
 }
 
-async function printDirectoryContents(directoryPath) {
+async function retrieveDirectoryContents(directoryPath) {
   try {
-    const files = fs.readdirSync(directoryPath); // Synchronous method
-    files.forEach((file) => {
-      console.log(`${file}`);
-    });
+    const files = await fs.readdir(directoryPath);
+    const all_files = files.join(" ");
+    return all_files;
   } catch (err) {
     console.error("Error reading directory:", err);
   }
 }
 
-async function textGeneration(context, task, output_constraint) {
-  console.log("prompt", prompt);
+async function analyze_text(context, task, output_constraint) {
   const response = await ai.models.generateContent({
     model: "gemini-2.0-flash",
     contents: `Context:${context}, Task:${task}, Output Constraint:${output_constraint}`,
@@ -58,17 +56,25 @@ async function textGeneration(context, task, output_constraint) {
       tools: [{ googleSearch: {} }],
     },
   });
-
   return response.text;
 }
 
-//printDirectoryContents("./pdfs");
-const response = await analyze_pdf(
-  "/pdfs/Curtis_Chen_2025_Resume_1.pdf",
-  "You are answering a question based on the pdf that is provided to you. If the information is not present in the pdf, say there is no answer.",
-  "Where has curtis worked at?",
-  "Answer in only text, no other formatting"
+const question = "Where has curtis worked at?";
+
+const directory_contents = await retrieveDirectoryContents("./pdfs");
+const directory_locating = await analyze_text(
+  "Given the question below along with all the files in this directory, choose which of the files will most likely have the answer,",
+  `Directory Contents: ${directory_contents}, Question: ${question}`,
+  "Answer with the file name only (one word response)"
 );
 
-//print_pdf("/pdfs/Curtis_Chen_2025_Resume_1.pdf");
-console.log("response", response);
+const pdf_analysis = await analyze_pdf(
+  "/pdfs/Curtis_Chen_2025_Resume_1.pdf",
+  "You are answering a question based on the pdf that is provided to you. If the information is not present in the pdf, say there is no answer in the pdf.",
+  `Question:${question}`,
+  "Answer in only text, no other formatting"
+);
+console.log("Question:", question);
+console.log("Directory Contents:", directory_contents);
+console.log("Direcotry Location for Question:", directory_locating);
+console.log("PDF Analysis:", pdf_analysis);
